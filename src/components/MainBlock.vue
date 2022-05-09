52614b6dd5d5cd813fc85673bbb55f3186b9c0fe
Vladislav Petrov <85939466+VlaDick2008@users.noreply.github.com>
2022-05-09 22:58:55 +0300

    Немного именил вещей

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -1,48 +1,74 @@
 <template>
   <div class="first-block rig">
-    <my-main-title>Landing template for startups</my-main-title>
-    <my-text class="my-4 w-1/2">
-      Our landing page template works on all devices, so you only have to set it up once, and get
-      beautiful results forever.
-    </my-text>
-    <my-button class="bg-button-color mr-3 mb-16">Start free trial</my-button>
-    <my-button class="bg-button-disabled-color text-opacity-70">Learn more</my-button>
-    <div class="video-block">
-      <img
-        @click="playVideo"
-        src="../assets/video-preview.png"
-        alt="video-preview"
-        class="video-preview"
-        id="video-preview"
-      />
-      <img @click="playVideo" id="play-btn" class="play-btn" src="../assets/play-btn.png" alt="" />
-      <iframe
-        id="yt-video"
-        class="hidden"
-        width="1024"
-        height="576"
-        src="https://www.youtube.com/embed/3ZEFD5tTRDI"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    </div>
+    <Transition name="title">
+      <my-main-title v-if="state.show">Landing template for startups</my-main-title>
+    </Transition>
+    <Transition name="subtitle">
+      <my-text v-if="state.show" class="my-4 w-1/2">
+        Our landing page template works on all devices, so you only have to set it up once, and get
+        beautiful results forever.
+      </my-text>
+    </Transition>
+
+    <TransitionGroup name="buttons">
+      <my-button v-if="state.show" class="bg-button-color mr-3 mb-16">Start free trial</my-button>
+      <my-button v-if="state.show" class="bg-button-disabled-color text-opacity-70"
+        >Learn more</my-button
+      >
+    </TransitionGroup>
+    <TransitionGroup name="video">
+      <div v-if="state.show" @click="playVideo" class="video-block">
+        <div v-if="!state.isPlayerVisible">
+          <img
+            @click="playVideo"
+            src="../assets/video-preview.png"
+            alt="video-preview"
+            class="video-preview"
+            id="video-preview"
+          />
+          <img
+            @click="playVideo"
+            id="play-btn"
+            class="play-btn"
+            src="../assets/play-btn.png"
+            alt=""
+          />
+        </div>
+        <iframe
+          v-else
+          id="yt-video"
+          width="1024"
+          height="576"
+          src="https://www.youtube.com/embed/3ZEFD5tTRDI"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </TransitionGroup>
   </div>
   <img class="secondary-shape" src="../assets/decor-shape-2.svg" alt="" />
 </template>
 
 <script setup lang="ts">
+import { onMounted, reactive } from 'vue';
+
+const state = reactive({
+  isPlayerVisible: false,
+  show: false,
+});
+
 const playVideo = () => {
   const video = document.querySelector('#yt-video');
-  const videoPreview = document.querySelector('#video-preview');
-  const videoPlayIco = document.querySelector('#play-btn');
 
   video?.setAttribute('src', 'https://www.youtube.com/embed/3ZEFD5tTRDI?;autoplay=1&;');
-  video?.classList.remove('hidden');
-  videoPreview?.classList.add('hidden');
-  videoPlayIco?.classList.add('hidden');
+  state.isPlayerVisible = true;
 };
+
+onMounted(() => {
+  state.show = true;
+});
 </script>
 
 <style lang="scss" scoped>
@@ -67,5 +93,36 @@ const playVideo = () => {
   @apply absolute z-0;
   left: 5%;
   top: 15%;
+}
+
+//Transitions
+.title-enter-active,
+.subtitle-enter-active,
+.buttons-enter-active,
+.video-enter-active {
+  transition: all 2s ease;
+  transform: translateY(0px);
+  transition-delay: 0.5s;
+}
+
+.title-enter-from,
+.subtitle-enter-from,
+.buttons-enter-from,
+.video-enter-from {
+  opacity: 0;
+  transform: translateY(50px);
+}
+
+.title-enter-active {
+  transition-delay: 0.5s;
+}
+.subtitle-enter-active {
+  transition-delay: 1s;
+}
+.buttons-enter-active {
+  transition-delay: 2s;
+}
+.video-enter-active {
+  transition-delay: 2.5s;
 }
 </style>
